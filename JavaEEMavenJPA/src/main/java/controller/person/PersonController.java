f25889dc3174ab4ec2b150693379a1c6d0c0f0d6
liaojames <liaojames@localhost>
2023-09-04 23:56:46 +0800

    SpringBootJPA

--- JavaEEMavenJPA/src/main/java/controller/person/PersonController.java
+++ JavaEEMavenJPA/src/main/java/controller/person/PersonController.java
@@ -10,26 +10,26 @@ public class PersonController {
 	public static void main(String[] args) {
 
 		// 1. Create
-		CRUDServices.create("mydb");
+//		CRUDServices.create("mydb");
 
 		// 2. For R-U-D
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("mydb");
 		EntityManager em = emf.createEntityManager();
 		
 		// Insert
-		CRUDServices.insertPersonInheritance(em, "James", "Liao", "C#");
-		CRUDServices.insertPersonInheritance(em, "David", "Nyon", "Java");
-		CRUDServices.insertPersonInheritance(em, "Tom", "Duck", "C++");
-		CRUDServices.insertPerson(em, "person", "Go");
-		CRUDServices.insertDeveloper(em, "Ajax");
+//		CRUDServices.insertPersonInheritance(em, "James", "Liao", "C#");
+//		CRUDServices.insertPersonInheritance(em, "David", "Nyon", "Java");
+//		CRUDServices.insertPersonInheritance(em, "Tom", "Duck", "C++");
+//		CRUDServices.insertPerson(em, "person", "Go");
+//		CRUDServices.insertDeveloper(em, "Ajax");
 
 	
 		// Query
-//		CRUDServices.queryPersonDataOnly(em, 1);
-//		CRUDServices.queryDeveloperDataOnly1(em, 2);
-//		CRUDServices.queryDeveloperDataOnly2(em, 2);
-//		CRUDServices.queryAllByNative(em);
-//		
+		CRUDServices.queryPersonDataOnly(em, 1);
+		CRUDServices.queryDeveloperDataOnly1(em, 2);
+		CRUDServices.queryDeveloperDataOnly2(em, 2);
+		CRUDServices.queryAllByNative(em);
+		
 //		// Delete
 //		CRUDServices.delete(em, 1);
 	}
